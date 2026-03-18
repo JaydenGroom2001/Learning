6558b6d52a93180d904c3480da93e13458faad4e
Jayden Groom <77188496+JaydenGroom2001@users.noreply.github.com>
2026-03-17 22:00:15 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log("Is this working");
+console.log("Why is this not working");
+console.log("How about now".length);
+
+const num1 = 4;
+let num3 = 8;
+const equals = num1 + num3;
+
+console.log(equals);
+
+// Self Information
+let name = "Jayden";
+let age = 26;
+let email = "Jayden.Groom2001@gmail.com";
+
+let me = `${name}, ${age}, ${email}`;
+
+console.log(me);
+
+let USD = 1200;
+let AUD = USD * 1.5;
+console.log("$" + AUD);
+
+
+if (age <= 25)
+{
+    console.log("You are too young");
+
+}else {
+    console.log("You are of age");
+};
